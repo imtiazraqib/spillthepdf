cec9de511164aaeb811cdf9c69ddf9c420efafe2
imtiazraqib <imtiazud@ualberta.ca>
2024-12-23 11:13:55 -0700

    implemented clerk functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["synthwave", "bumblebee"],
+    darkTheme: "synthwave",
+  },
 };
 export default config;
